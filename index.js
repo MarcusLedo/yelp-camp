d97c7c9299707e1f0bb5eddd03911c708a1d9352
Marcus <sabaledo27@hotmail.com>
2025-03-17 22:20:57 -0300

    Connect to mongodb and create our first scheme

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 import express from "express";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import mongoose from "mongoose";
+import { Campground } from "./models/campgroung.js";
 
 const __file = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__file);
@@ -11,9 +13,25 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
-  res.send("I'm alive");
+  res.render("dummy");
+});
+
+app.get("/makecampground", async (req, res) => {
+  const camp = new Campground({
+    title: "My Backyard",
+    description: "cheap camping!",
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
   console.log("Listening on port 3000.");
 });
+
+main().catch((err) => console.log("Oh no! Error " + err));
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
+  console.log("Mongo connected!");
+}
